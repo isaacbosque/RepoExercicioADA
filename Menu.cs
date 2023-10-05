7cbc690af1df00da817529930288999d61e3ebc5
Maurício <mauricio.mafrapedreiro@gmail.com>
2023-10-04 21:02:35 -0300

    Menu

---
+++ Menu.cs
@@ -0,0 +1,80 @@
+static void ImprimirMenu()
+{
+    Dictionary<int, string> opcoes = new Dictionary<int, string>();
+    int posicaoYMenu = Console.BufferHeight / 2;
+    int posicaoXMenu = Console.BufferWidth / 2;
+    int itemSelecionado = 0;
+
+    opcoes.Add(0, "Iniciar Jogo");
+    opcoes.Add(1, "Como jogar?");
+    opcoes.Add(2, "Sair");
+
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+    Console.CursorVisible = false;
+
+    while (true)
+    {
+        Console.Clear();
+        int posicao = CentralizarTexto("     __                    __       ____                 ");
+
+        EscreverNaPosicao(@"         __                    __       ____                 
+                             __ / /__  ___ ____    ___/ /__ _  / __/__  ___________ _
+                            / // / _ \/ _ `/ _ \  / _  / _ `/ / _// _ \/ __/ __/ _ `/
+                            \___/\___/\_, /\___/  \_,_/\_,_/ /_/  \___/_/  \__/\_,_/ 
+                                     /___/                                          ",
+         posicao,
+         5);
+
+        ImprimirControlesMenu();
+
+        Console.SetCursorPosition(Console.BufferWidth / 2, Console.BufferHeight / 2);
+        for (int i = 0; i < opcoes.Count; i++)
+        {
+            Console.SetCursorPosition(CentralizarTexto(opcoes[i]), (Console.BufferHeight / 2 - opcoes.Count + i));
+            if (itemSelecionado == i)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine(opcoes[i]);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+
+            }
+            else
+            {
+                Console.WriteLine(opcoes[i]);
+            }
+        }
+
+
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.UpArrow:
+                itemSelecionado--;
+
+                if (itemSelecionado < 0)
+                    itemSelecionado = opcoes.Count - 1;
+
+                break;
+            case ConsoleKey.DownArrow:
+                itemSelecionado++;
+                if (itemSelecionado == opcoes.Count)
+                    itemSelecionado = 0;
+                break;
+            case ConsoleKey.Enter:
+                if (itemSelecionado == 0)
+                    return;
+
+                if (itemSelecionado == 1)
+                    ImprimirComoJogar();
+
+                if (itemSelecionado == 2)
+                    System.Environment.Exit(1);
+                break;
+            default:
+                break;
+        }
+    }
+}
